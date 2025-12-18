7a81abecf10008775ff58ca85e0763ef278fd4ed
Nattapat Srirung <157050379+JeabtiE@users.noreply.github.com>
2025-12-18 23:15:36 +0700

    Update lab8_1.cpp

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,15 +3,30 @@ using namespace std;
 
 int main()
 {
-  char rank;
-  cout << "Input your rank: ";
-  cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
-  return 0;
+    char rank;
+    cout << "Input your rank: ";
+    cin >> rank;
+    
+    switch(rank) {
+        case 'S': 
+            cout << "You have received Super Ultra Rare Unit!!!\n";
+            break;
+        case 'A': 
+            cout << "You have received 5 gems.\n";
+            break;
+        case 'B': 
+            cout << "You have received 1 gems.\n";
+            break;
+        case 'C': 
+            cout << "You have received 2000 coins.\n";
+            break;
+        case 'D': 
+            cout << "You have received very KAK items.\n";
+            break;
+        default:
+            cout << "Invalid rank!\n";
+            break;
+    }
+    
+    return 0;
 }
