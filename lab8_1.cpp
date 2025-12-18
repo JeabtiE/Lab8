6afd1cffe3bf4d76063dd81d41b1572c039aa2ba
Nattapat Srirung <157050379+JeabtiE@users.noreply.github.com>
2025-12-18 23:59:17 +0700

    Refactor rank evaluation logic
    Consolidate rank checks into fewer conditional statements.

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -6,25 +6,22 @@ int main()
     char rank;
     cout << "Input your rank: ";
     cin >> rank;
-    
+
     if (rank == 'S') {
         cout << "You have received Super Ultra Rare Unit!!!\n";
     }
-    else if (rank == 'A') {
+    if (rank == 'S' || rank == 'A') {
         cout << "You have received 5 gems.\n";
     }
-    else if (rank == 'B') {
+    if (rank == 'S' || rank == 'A' || rank == 'B') {
         cout << "You have received 1 gems.\n";
     }
-    else if (rank == 'C') {
+    if (rank == 'S' || rank == 'A' || rank == 'B' || rank == 'C') {
         cout << "You have received 2000 coins.\n";
     }
-    else if (rank == 'D') {
+    if (rank == 'S' || rank == 'A' || rank == 'B' || rank == 'C' || rank == 'D') {
         cout << "You have received very KAK items.\n";
     }
-    else {
-        cout << "Invalid rank!\n";
-    }
-    
+
     return 0;
 }
